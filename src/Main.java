e7847441cf02a5bb6104914d4887b0e31f067909
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-01-31 12:10:54 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать");
+        }
+
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+    }
+
+
 }
