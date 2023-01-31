750c3bb4e31a8a030f3b9167a543fa0cc9616a18
Dmitrii Beliaev <dmitriymay97@gmail.com>
2023-01-31 12:24:06 +0300

    Задача 3 (в задаче 2 исправил знак с "больше или равно" на "больше")

--- src/Main.java
+++ src/Main.java
@@ -37,6 +37,7 @@ public class Main {
             System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + speed + ", то можно ездить спокойно");
+
         }
     }
 
